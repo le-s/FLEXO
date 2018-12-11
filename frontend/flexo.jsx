b750c0983a80732ca0d78272f010f4e10bcee608
Steven Le <steven.an.le@gmail.com>
2018-12-11 14:31:06 -0800

    Create user model

---
+++ frontend/flexo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById('root');
+  // const store = configureStore();  
+
+  ReactDOM.render(<h1>Turo Clone</h1>, root);
+})
