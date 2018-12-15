a70d15efde0a12157645d3577743e5df808e954e
Steven Le <steven.an.le@gmail.com>
2018-12-15 14:32:41 -0800

    Starting Active Storage

--- frontend/components/car/car_index_container.jsx
+++ frontend/components/car/car_index_container.jsx
@@ -3,12 +3,12 @@ import {connect} from 'react-redux';
 import {fetchCars, fetchCar} from '../../actions/car_actions';
 import CarIndex from './car_index';
 
-const mSTP = state => ({
-  cars: state.entities.cars
+const mSTP = (state, ownProps)=> ({
+  cars: state.entities.cars[ownProps.match.params.id]
 });
 
 const mDTP = dispatch => ({
+  fetchCar: id => dispatch(fetchCar(id))
+});
 
-})
-
-export default connect(mSTP, mDTP)(CarIndex)
+export default connect(mSTP, mDTP)(CarIndex);
