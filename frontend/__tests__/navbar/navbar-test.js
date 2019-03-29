41a654ad4d38c3909785c58a740713b99df051ac
Steven Le <steven.an.le@gmail.com>
2019-03-28 18:10:42 -0700

    Add jest testing to project

---
+++ frontend/__tests__/navbar/navbar-test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NavBar from './../../components/navbar/navbar';
+
+
+describe('NavBar component', () => {
+  it('renders without errors', () => {
+    expect(1).toBe(1);
+  });
+})
