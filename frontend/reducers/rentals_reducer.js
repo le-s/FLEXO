15064fa7d1cf978476fec3696a7930942df19571
Steven Le <steven.an.le@gmail.com>
2019-11-06 19:18:28 -0800

    Add reducer for user rentals

--- frontend/reducers/rentals_reducer.js
+++ frontend/reducers/rentals_reducer.js
@@ -1,9 +1,11 @@
-import { RECEIVE_RENTAL } from '../actions/rental_actions';
+import { RECEIVE_RENTAL, RECEIVE_RENTALS } from '../actions/rental_actions';
 
 const rentalsReducer = (state = {}, action) => {
   Object.freeze(state);
 
   switch (action.type) {
+    case RECEIVE_RENTALS:
+      return action.rentals;
     case RECEIVE_RENTAL:
       return Object.assign({}, state, { [action.rental.id]: action.rental });
     default:
